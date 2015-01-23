9239b73108636a55fce1e133754a5b5419d2e5c7
alysong24 <ang218@lehigh.edu>
2015-01-23 15:35:35 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+//Alyson Givre
+//Lab 1
+
+
+//Define a class
+public class HelloWorld {
+    
+    //Create main method
+    public static void main(String [] args){
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+    }
+    
+}
