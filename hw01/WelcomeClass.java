6474d4c95faa6d2fc749ade394d8602d79c520cc
alysong24 <ang218@lehigh.edu>
2015-01-24 03:02:03 +0000

    did hw1

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,21 @@
+//Alyson Givre
+//January 23, 2015
+//HW 1
+
+//define a class
+public class WelcomeClass {
+    
+    //create main method
+    public static void main(String [] args) {
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println(" ^  ^  ^  ^  ^  ^");
+        System.out.println(" / /\\ /\\ /\\ /\\ /\\ \\ ");
+        System.out.println("<-A--N--G--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("Over winter break I went to Costa Rica with my family.");
+    }
+
+}
